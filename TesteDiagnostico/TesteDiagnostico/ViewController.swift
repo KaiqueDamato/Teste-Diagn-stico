2802cbfcea1ddd5aa29ee255314a8708a99e7197
Kaique Pantosi D'amato <kaique_pantosi@hotmail.com>
2015-08-04 15:40:43 -0300

    navegacao tela principal

--- TesteDiagnostico/TesteDiagnostico/ViewController.swift
+++ TesteDiagnostico/TesteDiagnostico/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var blueView: UIView!
+    
+    lazy var views: [UIView!] = {
+        var viewsArray = [self.blueView, self.greenView, self.redView]
+        return viewsArray
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        //kaique
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,7 +27,30 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func moveRight(sender: UISwipeGestureRecognizer) {
+        moveRight()
+    }
+    
+    @IBAction func moveLeft(sender: UISwipeGestureRecognizer) {
+        moveLeft()
+    }
+    
+    func moveRight() {
+        UIView.animateWithDuration(0.3, animations: { () -> Void in
+            self.blueView.frame = CGRect(x: self.blueView.frame.origin.x + 300, y: self.blueView.frame.origin.y, width: 300, height: 300)
+            self.greenView.frame  = CGRect(x: self.greenView.frame.origin.x  + 300, y: self.greenView.frame.origin.y,  width: 300, height: 300)
+            self.redView.frame   = CGRect(x: self.redView.frame.origin.x   + 300, y: self.redView.frame.origin.y,   width: 300, height: 300)
+        })
+    }
+    
+    func moveLeft() {
+        UIView.animateWithDuration(0.3, animations: { () -> Void in
+            self.blueView.frame = CGRect(x: self.blueView.frame.origin.x - 300, y: self.blueView.frame.origin.y, width: 300, height: 300)
+            self.greenView.frame  = CGRect(x: self.greenView.frame.origin.x  - 300, y: self.greenView.frame.origin.y,  width: 300, height: 300)
+            self.redView.frame   = CGRect(x: self.redView.frame.origin.x   - 300, y: self.redView.frame.origin.y,   width: 300, height: 300)
+        })
+    }
 
 }
 
