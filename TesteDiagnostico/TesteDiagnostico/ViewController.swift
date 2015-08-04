b7c11e7c0d57555060463c64e2a724df12b71f37
Kaique Pantosi D'amato <kaique_pantosi@hotmail.com>
2015-08-04 14:31:29 -0300

    initial commit

---
+++ TesteDiagnostico/TesteDiagnostico/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TesteDiagnostico
+//
+//  Created by Kaique Damato on 8/4/15.
+//  Copyright (c) 2015 Kaique Damato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //kaique
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
